70274d89d010627c04e644cb41e38d1a2716c756
Smit Rupani <smit.24bcs10256@sst.scaler.com>
2026-08-18 16:45:36 +0530

    Variables

--- session3/variable.sh
+++ session3/variable.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-variable="Hello, World!"
-echo $variable
+# variable="Hello, World!"
+# echo $variable
 
 #ls="myfile" -- wrong way
 #we can't use commands names as a variable name
 
-name="Nensi"
-roll_no=123
-comment="Awesome"
+name="Smit"
+roll_no=10256
+comment="a DevOps Engineer"
 echo "My name is $name"
 echo "My roll number is $roll_no"
 echo "I am $comment"
