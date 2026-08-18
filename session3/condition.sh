2c751d008638f70ca6160f3840300218b6f62f10
Smit Rupani <smit.24bcs10256@sst.scaler.com>
2026-08-18 17:34:06 +0530

    conditons in bash

--- session3/condition.sh
+++ session3/condition.sh
@@ -2,11 +2,11 @@
 
 read -p "Enter your age: " age
 
-if [ $age -lt 0 ]; then
+if [ $age -le 0 ]; then
     echo "Invalid age. Please enter a valid age."
 elif [ $age -lt 13 ]; then
     echo "You are a child."
-elif [ $age -lt 20 ]; then
+elif [ $age -le 19 ]; then
     echo "You are a teenager."
 else
     echo "You are an adult."
